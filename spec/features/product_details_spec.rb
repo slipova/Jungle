f1269c81dfaa3a82f3df9b659b0fd18612924305
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2022-01-11 23:42:18 +0000

    implement test for product details page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "users navigate from home page to product detail page by clicking on a product", type: :feature, js: true do
+
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+
+    end
+  end
+
+  scenario "They see paroduct details page" do
+    # # ACT
+
+    visit root_path
+
+    page.find(".pull-right", match: :first).click()
+  
+    
+    # # DEBUG / VERIFY
+    
+    expect(page).to have_css 'section.products-show'
+    save_screenshot
+  end
+
 end
